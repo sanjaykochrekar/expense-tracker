7eaa770996ab12d55000b24417e20c0aab8b76e3
sanjay <kochrekarsanjay87@gmail.com>
2023-09-09 15:28:12 +0530

    initial setup

---
+++ Expense tracker/Expense_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_trackerApp.swift
+//  Expense tracker
+//
+//  Created by Sanju on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
