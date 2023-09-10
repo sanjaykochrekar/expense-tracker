5c1d4c013fdce752963c288b284392ec75eab8ab
sanjay <kochrekarsanjay87@gmail.com>
2023-09-10 10:36:59 +0530

    primary button background and foreground color fix

--- Expense tracker/Component/ETPrimaryButton.swift
+++ Expense tracker/Component/ETPrimaryButton.swift
@@ -12,6 +12,7 @@ struct ETPrimaryButton: View {
     var onClick: () -> Void
     
     private var backgroundColor: ETColor = .violet100
+    private var color: ETColor = .light100
     
     var body: some View {
        
@@ -22,11 +23,11 @@ struct ETPrimaryButton: View {
                     Spacer()
                     Text(label)
                         .font(Font.appFont(weight: .semiBold, size: 18))
-                        .foregroundColor(Color.appColor(.light100))
+                        .foregroundColor(Color.appColor(color))
                     Spacer()
                 }
                 .frame(height: 56)
-                .background(Color.appColor(.violet100))
+                .background(Color.appColor(backgroundColor))
             }
             .cornerRadius(16)
         
@@ -42,6 +43,12 @@ struct ETPrimaryButton: View {
         newView.backgroundColor = color
         return newView
     }
+    
+    func color(_ color: ETColor = .light100) -> Self {
+        var newView = self
+        newView.color = color
+        return newView
+    }
 }
 
 struct ETPrimaryButton_Previews: PreviewProvider {
@@ -49,6 +56,9 @@ struct ETPrimaryButton_Previews: PreviewProvider {
         ETPrimaryButton(label: "+ Add") {
             
         }
+        .color(.violet100)
+        .backgroundColor(.violet20)
         .padding(34)
+        
     }
 }
