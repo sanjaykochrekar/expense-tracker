8807ce93e1ae590d54a8e9a555b9f427dd373b7f
sanjay <kochrekarsanjay87@gmail.com>
2023-09-12 23:54:18 +0530

    add lock screen

--- Expense tracker/Screen/Lock/LockScreen.swift
+++ Expense tracker/Screen/Lock/LockScreen.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct LockScreen: View {
+    @State var pin: String = ""
+    
+    
+    private func moveNext() {
+        //
+    }
+    
     var body: some View {
         VStack {
             Spacer()
-            PinDot(total: 4, closed: 3)
+            PinDot(total: 4, closed: pin.count)
                 .frame(width: 120)
             Spacer()
             ETKeyboard { key in
-                print(key)
+                if key == .clear {
+                    pin = String(pin.dropLast(1))
+                } else if key == .next {
+                    moveNext()
+                } else if pin.count <= 4{
+                    pin = pin + key.rawValue
+                }
+                
             }
         }
         .background(Color.appColor(.violet100))
